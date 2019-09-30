9d9cfdfc8991148272e17717b2b327f4f70ec7ec
JamalGuyo <jamalguyo08@gmail.com>
2019-10-01 01:37:15 +0300

    create a state for the user's login - mask username toggle checkbox

--- src/app/user/login.component.ts
+++ src/app/user/login.component.ts
@@ -4,6 +4,9 @@ import { Router, ActivatedRoute } from '@angular/router';
 
 import { AuthService } from './auth.service';
 
+// NGRX
+import { Store, select } from '@ngrx/store';
+
 @Component({
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
@@ -14,12 +17,19 @@ export class LoginComponent implements OnInit {
 
   maskUserName: boolean;
 
-  constructor(private authService: AuthService,
-              private router: Router) {
-  }
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private store: Store<any>
+  ) {}
 
   ngOnInit(): void {
-
+    // TODO: unsubscribe
+    this.store.pipe(select('users')).subscribe(users => {
+      if (users) {
+        this.maskUserName = users.maskUserName;
+      }
+    });
   }
 
   cancel(): void {
@@ -27,7 +37,10 @@ export class LoginComponent implements OnInit {
   }
 
   checkChanged(value: boolean): void {
-    this.maskUserName = value;
+    this.store.dispatch({
+      type: 'MASK_USER_NAME',
+      payload: value
+    });
   }
 
   login(loginForm: NgForm): void {
--- src/app/user/state/user.reducer.ts
+++ src/app/user/state/user.reducer.ts
@@ -1,9 +1,9 @@
 export function reducer(state, action) {
   switch (action.type) {
-    case 'TOGGLE_PRODUCT_CODE':
+    case 'MASK_USER_NAME':
       return {
         ...state,
-        showProductCode: action.payload
+        maskUserName: action.payload
       };
     default:
       return state;
