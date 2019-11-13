05debddb936ffb0d80f494e0c7f6bbc3340db730
JamalGuyo <jamalguyo08@gmail.com>
2019-11-13 14:45:57 +0300

    strongly type actions with action creators

--- src/app/products/state/product.action.ts
+++ src/app/products/state/product.action.ts
@@ -9,18 +9,21 @@ export enum ProductActionTypes {
   InitializeCurrentProduct = '[Product] Initialize Current Product',
   Load = '[Product] Load',
   LoadSuccess = '[Product] Load Success',
-  LoadFail = '[Product] Load Fail'
+  LoadFail = '[Product] Load Fail',
+  UpdateProduct = '[Product] Update Product',
+  UpdateProductSuccess = '[Product] Update Product Success',
+  UpdateProductFail = '[Product] Update Product Fail'
 }
 
 // action creators
 export class ToggleProductCode implements Action {
   readonly type = ProductActionTypes.ToggleProductCode;
-  constructor(public payload: boolean) {}
+  constructor(public payload: boolean) { }
 }
 
 export class SetCurrentProduct implements Action {
   readonly type = ProductActionTypes.SetCurrentProduct;
-  constructor(public payload: Product) {}
+  constructor(public payload: Product) { }
 }
 
 export class ClearCurrentProduct implements Action {
@@ -37,12 +40,27 @@ export class Load implements Action {
 
 export class LoadSuccess implements Action {
   readonly type = ProductActionTypes.LoadSuccess;
-  constructor(public payload: Product[]) {}
+  constructor(public payload: Product[]) { }
 }
 
 export class LoadFail implements Action {
   readonly type = ProductActionTypes.LoadFail;
-  constructor(public payload: string) {}
+  constructor(public payload: string) { }
+}
+
+export class UpdateProduct implements Action {
+  readonly type = ProductActionTypes.UpdateProduct;
+  constructor(public payload: Product) { }
+}
+
+export class UpdateProductSuccess implements Action {
+  readonly type = ProductActionTypes.UpdateProductSuccess;
+  constructor(public payload: Product) { }
+}
+
+export class UpdateProductFail implements Action {
+  readonly type = ProductActionTypes.UpdateProductFail;
+  constructor(public payload: string) { }
 }
 
 // union type
@@ -53,4 +71,7 @@ export type ProductActions =
   | InitializeCurrentProduct
   | Load
   | LoadSuccess
-  | LoadFail;
+  | LoadFail
+  | UpdateProduct
+  | UpdateProductSuccess
+  | UpdateProductFail;
